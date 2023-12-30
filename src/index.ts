a8aac06d154f8fc82ca1030845e79a97ea5b2c34
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2023-12-30 18:46:35 +0530

    feat: add currencies api router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express from "express";
-import { config } from "dotenv";
+import express from 'express';
+import { config } from 'dotenv';
+import currencyRouter from 'routes/currencies.route';
 
 config();
 
@@ -8,10 +9,13 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // index route and also to health check
-app.get("/", (_req, res) => {
-  res.send("Hello From CryptoFiat server!");
+app.get('/', (_req, res) => {
+  res.send('Hello From CryptoFiat server!');
 });
 
+// api routers
+app.use('/api/v1/currencies', currencyRouter);
+
 app.listen(PORT, () => {
-  console.log("Server is running on port: ", PORT);
+  console.log('Server is running on port: ', PORT);
 });
