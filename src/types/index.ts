d9d1a1a95156dabdd810b383d26eba2f0177db7d
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2023-12-30 19:00:27 +0530

    feat: define types

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export type CryptoCurrency = {
+  id: number;
+  name: string;
+  symbol: string;
+  sign: string;
+};
+
+export type CryptoInfo = {
+  id: number;
+  name: string;
+  logo: string;
+};
+
+export type FiatCurrency = {
+  id: number;
+  name: string;
+  sign: string;
+  symbol: string;
+};
