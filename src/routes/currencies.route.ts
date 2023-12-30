f71083b33d286f7eac528b9cd4037e663a7f100b
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2023-12-30 18:24:10 +0530

    feat: add currencies router and listcurrencies api route

---
+++ src/routes/currencies.route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { listCurrenciesHandler } from 'controllers/currencies.controller';
+
+const router = Router();
+
+router.get('/', listCurrenciesHandler);
+
+export default router;
